190acf911876be0b29fc97522b3b1ef4c770dcd0
Hugo Diniz <hhldiniz@gmail.com>
2017-11-25 12:54:57 -0300

    adição de controller para banco de dados

---
+++ DBController.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+
+class DBController:
+    def __init__(self, host, db_name, db_user, db_pass):
+        try:
+            self.__con = psycopg2.connect(f"dbname={db_name}, user={db_user}, host={host}, password={db_pass}")
+            self.__cursor = self.__con.cursor()
+        except:
+            print("Conexão com o banco falhou")
+
+    def insert_data(self, table="", fields="", data=""):
+        self.__cursor.execute(f"INSERT INTO {table}({fields}) VALUES ({data})")
+        return self.__cursor.fetchall()
+
+    def select_data(self, table="", fields="", conditions="WHERE 1"):
+        self.__cursor.execute(f"SELECT {fields} FROM {table} {conditions}")
+        return self.__cursor.fetchall()
+
+    def update_data(self, table="", field="", value="", conditions="WHERE 1"):
+        self.__cursor.execute(f"UPDATE {table} SET {field}={value} {conditions}")
+        return self.__cursor.fetchall()
