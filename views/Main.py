54d2efd2f25f09ab187389a018b45aeddd345c08
hugo <hhldiniz@gmail.com>
2017-11-28 09:45:05 -0300

    correção de bugs

--- views/Main.py
+++ views/Main.py
@@ -5,12 +5,13 @@ from views.View import View
 class Main(View):
     def __init__(self, title="Poopt Bank", size="800x600"):
         View.__init__(self, title, size)
+        View.set_btn_callback(self, self.__btn_callback)
 
     def __btn_callback(self, btn):
         if btn == "Entrar":
             db_controller = DBController("localhost", "poopt", "postgres", "cgch36AA")
             data = db_controller.select_data("user", "username,password")
         elif btn == "Sair":
-            View.get_app_gui(self).__app.stop()
+            View.get_app_gui(self).stop()
         elif btn == "Cadastrar":
             pass
--- views/View.py
+++ views/View.py
@@ -3,7 +3,11 @@ from appJar import gui
 
 class View:
     def __init__(self, title, size):
-        self.app = gui(title, size)
+        self.__app = gui(title, size)
+        self.__btn_callback = None
+
+    def set_btn_callback(self, callback):
+        self.__btn_callback = callback
 
     def add_label(self, label_name, label_text):
         self.__app.addLabel(label_name, label_text)
@@ -19,6 +23,9 @@ class View:
 
     def add_secret_entries(self, entry_name):
         self.__app.addLabelSecretEntry(entry_name)
+
+    def __btn_callback(self, btn):
+        pass
 
     def get_app_gui(self):
         return self.__app
