2f611c2b1e49f765556e1b0e1e659da2403cc2a4
hugo <hhldiniz@gmail.com>
2017-11-28 16:19:03 -0300

    criando classe generica modal e modal de cadastro

--- views/Modal.py
+++ views/Modal.py
@@ -1,3 +1,7 @@
 class ModalWindow:
-    def __init__(self):
+    def __init__(self, app, title):
         self.__modal = self
+        self.__app = app
+        self.__title = title
+        self.__app.startSubWindow(title, modal=True)
+        self.__app.showSubWindow(title)
--- views/Signup.py
+++ views/Signup.py
@@ -3,11 +3,9 @@ from views.Modal import ModalWindow
 
 class Signup(ModalWindow):
     def __init__(self, app, title="Poopt Signup"):
-        ModalWindow.__init__(self)
-        self.__app = app
-        self.__title = title
-        self.__app.startSubWindow(title, modal=True)
-        self.__app.showSubWindow(title)
+        ModalWindow.__init__(self, app, title)
+        app.addValidationEntry("New Username")
+        app.addSecretLabelEntry("New Password")
 
     def destroy_window(self):
         self.__app.destroySubWindow(self.__title)
