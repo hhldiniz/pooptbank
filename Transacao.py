b332f59207c54cdb108093fc43fb98d7ad799038
hugo <hhldiniz@gmail.com>
2017-12-08 16:46:58 -0300

    método para execução de transação

--- Transacao.py
+++ Transacao.py
@@ -1,8 +1,12 @@
+from DBController import DBController
+
 class Transacao:
-    def __init__(self, data, descricao, valor):
+    def __init__(self, data, descricao, valor, user1, user2):
         self.__data = data
         self.__descricao = descricao
         self.__valor = valor
+        self.__user1 = user1
+        self.__user2 = user2
 
     def get_data(self):
         return self.__data
@@ -21,3 +25,14 @@ class Transacao:
 
     def set_valor(self, valor):
         self.__valor = valor
+
+    def execute(self):
+        try:
+            db_controller = DBController()
+            user1_balance = db_controller.select_data_single("users", {"username": self.__user1})['balance']
+            user2_balance = db_controller.select_data_single("users",  {"username": self.__user2})['balance']
+            db_controller.update_data("users", {'username':self.__user1}, {'balance': user1_balance - self.get_valor()})
+            db_controller.update_data("users", {'username': self.__user2}, {'balance': user2_balance + self.get_valor()})
+            return True
+        except:
+            return False
